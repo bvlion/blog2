d587d5c20dab95d6d393a4377a63d0aaef4918f8
bvlion <bvltiggeari@ambitious-i.net>
2024-03-07 12:07:41 +0900

    `/posts/` から `/blog/` に変更

--- update_html.sh
+++ update_html.sh
@@ -3,4 +3,3 @@
 cp repleace_files/404.html themes/BlogRa/layouts/.
 cp repleace_files/index.html themes/BlogRa/layouts/.
 cp repleace_files/terms.html themes/BlogRa/layouts/_default/.
-cp repleace_files/list.html themes/BlogRa/layouts/posts/.
