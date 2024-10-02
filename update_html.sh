17979a664cd6092e08690f377dee3931535f606c
bvlion <bvltiggeari@ambitious-i.net>
2024-10-02 12:55:29 +0900

    update deprecated method

--- update_html.sh
+++ update_html.sh
@@ -3,3 +3,4 @@
 cp repleace_files/404.html themes/BlogRa/layouts/.
 cp repleace_files/index.html themes/BlogRa/layouts/.
 cp repleace_files/terms.html themes/BlogRa/layouts/_default/.
+cp repleace_files/disqus.html themes/BlogRa/layouts/partials/.
