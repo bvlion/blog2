3a328b4d8ebc7ccd19499eaf0c066b824d596705
Satoshi Iwai <24517539+bvlion@users.noreply.github.com>
2026-08-19 14:28:00 +0900

    Update for Hugo 0.165 and clean up layout overrides (#67)

--- update_html.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cp repleace_files/404.html themes/BlogRa/layouts/.
-cp repleace_files/index.html themes/BlogRa/layouts/.
-cp repleace_files/terms.html themes/BlogRa/layouts/_default/.
-cp repleace_files/disqus.html themes/BlogRa/layouts/partials/.
